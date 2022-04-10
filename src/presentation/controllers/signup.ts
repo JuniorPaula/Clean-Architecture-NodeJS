3d2f3dbe795016550e7fb9d41823b559288623ad
Junior Paula <luke.junnior@icloud.com>
2022-04-10 10:25:13 -0300

    test: ensure SignUpController retuns 400 if password confirmation fails

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -12,20 +12,20 @@ export class SignUpController implements Controller {
 
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
-      const requiredFields = ['name', 'email', 'password', 'passwordConfimation']
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamsError(field))
         }
       }
 
-      if (httpRequest.body.password !== httpRequest.body.passwordConfimation) {
-        return badRequest(new InvalidParamsError('passwordConfimation'))
-      }
-
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!isValid) {
         return badRequest(new InvalidParamsError('email'))
+      }
+
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamsError('passwordConfirmation'))
       }
     } catch (error) {
       return serverError()
