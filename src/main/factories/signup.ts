6c0bd816f462e62e049c04b12de17685cfc406c1
Junior Paula <slucivaldo509@gmail.com>
2022-05-01 10:47:50 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,17 @@
+import { DbAddAccount } from '../../data/usecase/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+
+export const makeSignUpController = (): SignUpController => {
+  const salt = 12
+
+  /** Dependency ijenctions */
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  const accountMongoRepository = new AccountMongoRepository()
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+
+  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+}
