3a6adf247d612485270245feb5211bbb4862f3c1
Junior Paula <slucivaldo509@gmail.com>
2022-05-22 10:49:28 -0300

    test: ensure ValidationComposite returns the first error if more than one validation fails

--- src/presentation/helpers/validations/validation-compasite.spec.ts
+++ src/presentation/helpers/validations/validation-compasite.spec.ts
@@ -3,35 +3,43 @@ import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
 const makeValidationStub = (): Validation => {
-  class ValidationStub implements Validation {
+  class ValidationStubs implements Validation {
     validate (input: any): Error {
       return null
     }
   }
 
-  return new ValidationStub()
+  return new ValidationStubs()
 }
 
 interface SutTypes {
   sut: ValidationComposite
-  validationStub: Validation
+  validationStubs: Validation[]
 }
 
 const makeSut = (): SutTypes => {
-  const validationStub = makeValidationStub()
-  const sut = new ValidationComposite([validationStub])
+  const validationStubs = [makeValidationStub(), makeValidationStub()]
+  const sut = new ValidationComposite(validationStubs)
 
   return {
     sut,
-    validationStub
+    validationStubs
   }
 }
 
 describe('Validation Composite', () => {
   test('Should returns an error if any validation fails', async () => {
-    const { sut, validationStub } = makeSut()
-    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamsError('field'))
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new MissingParamsError('field'))
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new MissingParamsError('field'))
+  })
+
+  test('Should returns the first error if more than one validation fails', async () => {
+    const { sut, validationStubs } = makeSut()
+    jest.spyOn(validationStubs[0], 'validate').mockReturnValueOnce(new Error())
+    jest.spyOn(validationStubs[1], 'validate').mockReturnValueOnce(new MissingParamsError('field'))
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toEqual(new Error())
   })
 })
