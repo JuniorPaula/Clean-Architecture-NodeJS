0e3a80001c555b794b4528b82639823361211748
Junior Paula <slucivaldo509@gmail.com>
2022-05-23 19:52:10 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/protocols/load-acccount-by-email-repository.ts
@@ -0,0 +1,5 @@
+import { AccountModel } from '../usecase/add-account/db-add-account-protocols'
+
+export interface LoadAccountByEmailRepository {
+  load: (email: string) => Promise<AccountModel>
+}
