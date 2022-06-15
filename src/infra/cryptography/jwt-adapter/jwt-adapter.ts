696f7c8f2c200c06124efcb49555529fbfd8fbe5
Junior Paula <slucivaldo509@gmail.com>
2022-06-15 19:59:58 -0300

    feat: ensure JwtAdapter calls varify with correct values

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken'
+import { Decrypter } from '../../../data/protocols/cryptograthy/decryper'
 import { TokenGenerator } from '../../../data/protocols/cryptograthy/token-generator'
 
-export class JwtAdapter implements TokenGenerator {
+export class JwtAdapter implements TokenGenerator, Decrypter {
   private readonly secret: string
   constructor (secret: string) {
     this.secret = secret
@@ -10,5 +11,10 @@ export class JwtAdapter implements TokenGenerator {
   async generate (id: string): Promise<string> {
     const accessToken = await jwt.sign({ id }, this.secret)
     return accessToken
+  }
+
+  async decrypt (value: string): Promise<string> {
+    jwt.verify(value, this.secret)
+    return null
   }
 }
