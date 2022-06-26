32950bb54b1bca1f528f3667880d4c43800e1466
Junior Paula <slucivaldo509@gmail.com>
2022-06-26 09:50:54 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult if there is no data

---
+++ src/infra/db/mongodb/survey-result/survey-reault-mongo-reposiotory.ts
@@ -0,0 +1,25 @@
+import {
+  SaveSurveyResultModel,
+  SaveSurveyResultRepository,
+  SurveyResultModel
+} from '@/data/usecase/save-survey-result/db-save-survey-result-protocols'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    const response = await surveyResultCollection.findOneAndUpdate({
+      surveyId: data.surveyId,
+      accountId: data.accountId
+    }, {
+      $set: {
+        answer: data.answer,
+        date: data.date
+      }
+    }, {
+      upsert: true,
+      returnOriginal: false
+    })
+    return response.value && MongoHelper.map(response.value)
+  }
+}
