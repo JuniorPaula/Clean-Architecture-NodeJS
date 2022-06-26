c1d21ca9c7a8e4abc32f899b99c62ac48f1141ae
Junior Paula <slucivaldo509@gmail.com>
2022-06-26 14:46:45 -0300

    test: ensure save survey result routes returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,6 +1,12 @@
 import request from 'supertest'
+import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import { app } from '@/main/config/app'
+import env from '@/main/config/env'
+
+let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Result Routes', () => {
   beforeAll(async () => {
@@ -11,6 +17,13 @@ describe('Survey Result Routes', () => {
     await MongoHelper.disconnect()
   })
 
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   describe('PUT /surveys/:surveyId/results', () => {
     test('Should return 403 on save surveys result without accessToken', async () => {
       await request(app)
@@ -19,6 +32,42 @@ describe('Survey Result Routes', () => {
           answer: 'any_answer'
         })
         .expect(403)
+    })
+
+    test('Should return 200 on save survey result with accessToken', async () => {
+      const account = await accountCollection.insertOne({
+        name: 'John Due',
+        email: 'john@mail.com',
+        password: '123'
+      })
+      const id = account.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+
+      await request(app)
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        .put(`/api/surveys/${res.ops[0]._id}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'Answer 1'
+        })
+        .expect(200)
     })
   })
 })
